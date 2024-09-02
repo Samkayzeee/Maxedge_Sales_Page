c353544779f099a0c7baae30c761875ccca1310b
Abdulsamad <lasisiabdulsamad9@gmail.com>
2024-09-02 21:33:48 +0100

    making featured products to not overlay navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const link = [
 
 const Navbar = () => {
     return ( 
-        <div className="navbar bg-gray-500/30 backdrop-blur-xl sticky top-0 z-10">
+        <div className="navbar bg-gray-500/30 backdrop-blur-xl sticky top-0 z-30">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl text-black" href="#home">Logo.</a>
         </div>
